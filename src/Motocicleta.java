a189699465bcefa3ff784e1d437286bcac04d8b6
jaysonmelo@gmail.com <jaysonmelo@gmail.com>
2022-03-13 20:18:44 -0300

    Exemplo prático 02

---
+++ src/Motocicleta.java
@@ -0,0 +1,13 @@
+
+public class Motocicleta {
+
+	public String marca;
+	public String modelo;
+	
+	public Motocicleta(String marcaDaMoto, String modeloDaMoto) {
+		this.marca = marcaDaMoto;
+		this.modelo = modeloDaMoto;
+	}
+	
+	
+}
---
+++ src/Principal.java
@@ -0,0 +1,13 @@
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		Motocicleta moto = new Motocicleta("HONDA", "CB 500");
+		
+		System.out.println("Marca: " + moto.marca + " MODELO: " + moto.modelo);
+		
+		
+	}
+	
+}
